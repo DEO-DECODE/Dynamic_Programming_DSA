5a59a7a45a24504de8532477096f1cccef9d078f
DEO-DECODE <106802468+DEO-DECODE@users.noreply.github.com>
2023-02-23 10:20:03 +0530

    Create Form a palindrome(GFG).cpp

---
+++ Form a palindrome(GFG).cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Form a palindrome(GFG)
+class Solution
+{
+public:
+    int minInsertions(string s)
+    {
+        string str1 = s;
+        int n = s.length();
+        reverse(s.begin(), s.end());
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        // return f(n - 1, n - 1, dp, str1, s);
+        // base case
+        for (int i = 0; i <= n; ++i)
+            dp[i][0] = 0;
+        for (int j = 0; j <= n; ++j)
+            dp[0][j] = 0;
+        for (int i = 1; i <= n; ++i)
+        {
+            for (int j = 1; j <= n; ++j)
+            {
+                if (str1[i - 1] == s[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return str1.length() - dp[n][n];
+    }
+
+    int countMin(string str)
+    {
+        return minInsertions(str);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string str;
+        cin >> str;
+        Solution ob;
+        cout << ob.countMin(str) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
