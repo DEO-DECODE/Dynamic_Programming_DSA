4659e7b7cd38584986b588bf59dbbd0f6c1c1088
DEO-DECODE <106802468+DEO-DECODE@users.noreply.github.com>
2023-02-28 17:25:46 +0530

    Create Best Time to Buy and Sell Stock IV(LeetCode).cpp

---
+++ Best Time to Buy and Sell Stock IV(LeetCode).cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Best Time to Buy and Sell Stock IV(LeetCode)
+int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+    vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1, 0)));
+    for (int ind = n - 1; ind >= 0; --ind)
+    {
+        for (int buy = 0; buy <= 1; ++buy)
+        {
+            for (int cap = 1; cap <= k; ++cap)
+            {
+                // int profit = 0;
+                if (buy == 1)
+                {
+                    dp[ind][buy][cap]= max(-prices[ind] + dp[ind + 1][0][cap], 0 + dp[ind + 1][1][cap]);
+                }
+                else{
+                    dp[ind][buy][cap]= max(+prices[ind] + dp[ind + 1][1][cap-1], 0 + dp[ind + 1][0][cap]);
+                }
+            }
+        }
+    }
+    return dp[0][1][k];
+    }
