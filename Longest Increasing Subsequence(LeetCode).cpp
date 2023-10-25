b7ffe36e74e27cf64ab55bbb19a33bc3a08f61e8
DEO-DECODE <106802468+DEO-DECODE@users.noreply.github.com>
2023-10-26 01:30:18 +0530

    Update Longest Increasing Subsequence(LeetCode).cpp

--- Longest Increasing Subsequence(LeetCode).cpp
+++ Longest Increasing Subsequence(LeetCode).cpp
@@ -24,3 +24,20 @@ int lengthOfLIS(vector<int> &nums)
     vector<vector<int>> dp(n, vector<int>(n + 1, -1));
     return f(0, -1, nums, dp);
 }
+    int lengthOfLIS2(vector<int>& nums) {
+         int n = nums.size();
+  vector<int> dp(n, 1);
+  int maxm = 1;
+  for (int i = 0; i < n; ++i)
+  {
+    for (int prev = 0; prev < i; ++prev)
+    {
+      if (nums[i] > nums[prev])
+      {
+        dp[i] = max(dp[i], 1 + dp[prev]);
+      }
+    }
+    maxm=max(maxm, dp[i]);
+  }
+  return maxm;
+    }
